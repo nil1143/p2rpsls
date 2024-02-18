3dfd0c68149bedb628d684346d9d2f403c5f27cf
Tom Nil <tnil1143@gmail.com>
2024-02-18 02:19:15 +0000

    computer choice function

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,9 +6,21 @@ const resultMessage = document.getElementById('result');
 let userScore = 0;
 let computerScore = 0;
 
+
+// GAME
+
+// My choice listener with arrow function
 choices.forEach(choice => {
     choice.addEventListener('click', () => {
         const userChoice = choice.getAttribute('data-choice')
         console.log(userChoice)
     })
 })
+
+// Computer choice
+function getComputerChoice() {
+    const compChoices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+    const randomIndex = Math.floor(Math.random() * compChoices.length);
+    return compChoices[randomIndex]
+}
+console.log(getComputerChoice())
