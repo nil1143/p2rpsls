5c0ee0addb4555ea06dc510d34ebf200a922aa8e
Tom Nil <tnil1143@gmail.com>
2024-02-18 01:24:18 +0000

    handling game elements in js

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+const choices = document.querySelectorAll('.choice');
+const userScoreSpan = document.getElementById('user-score');
+const computerScoreSpan = document.getElementById('computer-score');
+const resultMessage = document.getElementById('result');
+
+let userScore = 0;
+let computerScore = 0;
